e4e5353e7f9d23a87bfbb22e9eb0ea82fd1de27f
iamqwertyloal <iamqwertyloal@gmail.com>
2023-01-25 20:20:01 +0500

    Add TailwindCSS to the project

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,vue,ts,jsx,tsx,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
