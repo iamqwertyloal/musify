e252d745f00d03f0d8a9031333c03c0566e38d78
iamqwertyloal <iamqwertyloal@gmail.com>
2023-02-01 17:37:33 +0500

    Add Manage component, add names to routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
+import ManageView from "../views/ManageView.vue";
 
 const routes = [
   {
+    name: "home",
     path: "/",
     component: HomeView,
   },
   {
+    name: "about",
     path: "/about",
     component: AboutView,
+  },
+  {
+    name: "manage",
+    path: "/manage",
+    component: ManageView,
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  linkExactActiveClass: "text-yellow-500",
 });
 
 export default router;
