a64e2728f7f58287344f0d62dfa90c8c2650670a
iamqwertyloal <iamqwertyloal@gmail.com>
2023-02-05 23:55:28 +0500

    Add form validation to CompositionItem form

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -1,8 +1,8 @@
 <template>
   <!-- Composition Items -->
   <div class="border border-gray-200 p-3 mb-4 rounded">
-    <div>
-      <h4 class="inline-block text-2xl font-bold">Song Name</h4>
+    <div v-show="!showForm">
+      <h4 class="inline-block text-2xl font-bold">{{ song.modified_name }}</h4>
       <button
         class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
       >
@@ -10,27 +10,36 @@
       </button>
       <button
         class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
+        @click.prevent="showForm = !showForm"
       >
         <i class="fa fa-pencil-alt"></i>
       </button>
     </div>
-    <div>
-      <form>
+    <div v-show="showForm">
+      <vee-form
+        :validation-schema="schema"
+        :initial-values="song"
+        @submit="edit"
+      >
         <div class="mb-3">
           <label class="inline-block mb-2">Song Title</label>
-          <input
+          <vee-field
+            name="modified_name"
             type="text"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
             placeholder="Enter Song Title"
           />
+          <ErrorMessage class="text-red-600" name="modified_name" />
         </div>
         <div class="mb-3">
           <label class="inline-block mb-2">Genre</label>
-          <input
+          <vee-field
+            name="genre"
             type="text"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
             placeholder="Enter Genre"
           />
+          <ErrorMessage class="text-red-600" name="genre" />
         </div>
         <button
           type="submit"
@@ -44,7 +53,7 @@
         >
           Go Back
         </button>
-      </form>
+      </vee-form>
     </div>
   </div>
 </template>
@@ -56,6 +65,20 @@ export default {
     song: {
       type: Object,
       required: true,
+    },
+  },
+  data() {
+    return {
+      showForm: false,
+      schema: {
+        modified_name: "required",
+        genre: "alpha_spaces",
+      },
+    };
+  },
+  methods: {
+    edit() {
+      console.log("Song edited!");
     },
   },
 };
