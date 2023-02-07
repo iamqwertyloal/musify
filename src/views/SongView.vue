e4617b45b34fa4f7501e209fec3c97aa5c8bd6cc
iamqwertyloal <iamqwertyloal@gmail.com>
2023-02-07 23:21:54 +0500

    Add functionality to sort comments

--- src/views/SongView.vue
+++ src/views/SongView.vue
@@ -58,6 +58,7 @@
         </vee-form>
         <!-- Sort Comments -->
         <select
+          v-model="sort"
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
         >
           <option value="1">Latest</option>
@@ -70,7 +71,7 @@
   <ul class="container mx-auto">
     <li
       class="p-6 bg-gray-50 border border-gray-200"
-      v-for="comment in comments"
+      v-for="comment in sortedComments"
       :key="comment.docID"
     >
       <!-- Comment Author -->
@@ -99,6 +100,7 @@ export default {
     return {
       song: {},
       comments: [],
+      sort: "1",
       schema: {
         comment: "required|min:3",
       },
@@ -110,6 +112,15 @@ export default {
   },
   computed: {
     ...mapState(useUserStore, ["userLoggedIn"]),
+    sortedComments() {
+      return this.comments.slice().sort((a, b) => {
+        if (this.sort === "1") {
+          return new Date(b.datePosted) - new Date(a.datePosted);
+        }
+
+        return new Date(a.datePosted) - new Date(b.datePosted);
+      });
+    },
   },
   async created() {
     const docSnapshot = await songsCollection.doc(this.$route.params.id).get();
@@ -139,6 +150,8 @@ export default {
       };
 
       await commentsCollection.add(comment);
+
+      this.getComments();
 
       this.comment_in_submission = false;
       this.comment_alert_variant = "bg-green-500";
