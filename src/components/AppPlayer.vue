c6b9813b304179f656f5522e838c3bf5a5f039fe
iamqwertyloal <iamqwertyloal@gmail.com>
2023-02-08 20:48:27 +0500

    Add functionality for song rewinding
    User can rewind songs by clicking on progress bar

--- src/components/AppPlayer.vue
+++ src/components/AppPlayer.vue
@@ -18,7 +18,10 @@
       <!-- Current Position -->
       <div class="player-currenttime">{{ seek }}</div>
       <!-- Scrub Container  -->
-      <div class="w-full h-2 rounded bg-gray-200 relative cursor-pointer">
+      <div
+        class="w-full h-2 rounded bg-gray-200 relative cursor-pointer"
+        @click.prevent="updateSeek"
+      >
         <!-- Player Ball -->
         <span
           class="absolute -top-2.5 -ml-2.5 text-gray-800 text-lg"
@@ -45,7 +48,7 @@ import usePlayerStore from "../stores/player";
 export default {
   name: "AppPlayer",
   methods: {
-    ...mapActions(usePlayerStore, ["toggleAudio"]),
+    ...mapActions(usePlayerStore, ["toggleAudio", "updateSeek"]),
   },
   computed: {
     ...mapState(usePlayerStore, [
